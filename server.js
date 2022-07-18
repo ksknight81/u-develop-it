9ffc22bc40fefaab1b30eb64f2c18d8dfa42ce57
Kevin Knight <ksknight81@gmail.com>
2022-07-17 20:50:13 -0400

    Added ability to console log rows

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const mysql = require('mysql2');
+
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
@@ -7,12 +9,29 @@ const app = express();
 app.use(express.urlencoded( { extended: false }));
 app.use(express.json());
 
+// connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        //your mysql username,
+        user: 'root',
+        // your mysql password
+        password: 'Che$terC@t123',
+        database: 'election'
+    },
+    console.log('Connected to the election database. ')
+);
+
 //test to see if the connection is workign with express
 // app.get('/', (req, res) => {
 //     res.json({
 //         message: 'Hello World'
 //     });
 // });
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 // default response for any other request (not found)
 app.use((req, res) => {
